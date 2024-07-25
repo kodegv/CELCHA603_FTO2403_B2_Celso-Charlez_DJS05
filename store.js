fc0789588f230e40d0afc463f8419cc3a6151bf9
kodegv <celsocharlez1@gmail.com>
2024-07-25 11:30:42 +0200

    export store actions and state change functions

--- store.js
+++ store.js
@@ -10,29 +10,29 @@ const RESET = 'RESET';
 
 // Define the reducer function
 function reducer(state = initialState, action) {
-    switch (action.type) {
-      case ADD:
-        return { count: state.count + 1 };
-      case SUBTRACT:
-        return { count: state.count - 1 };
-      case RESET:
-        return { count: 0 };
-      default:
-        return state;
-    }
+  switch (action.type) {
+    case ADD:
+      return { count: state.count + 1 };
+    case SUBTRACT:
+      return { count: state.count - 1 };
+    case RESET:
+      return { count: 0 };
+    default:
+      return state;
+  }
+}
+
+// Create the store
+function createStore(reducer) {
+  let state;
+  let listeners = [];
+
+  // Get the current state
+  function getState() {
+    return state;
   }
 
-  // Create the store
-function createStore(reducer) {
-    let state;
-    let listeners = [];
-  
-    // Get the current state
-    function getState() {
-      return state;
-    }
-
-    // Dispatch an action
+  // Dispatch an action
   function dispatch(action) {
     state = reducer(state, action);
     listeners.forEach(listener => listener());
@@ -45,3 +45,20 @@ function createStore(reducer) {
       listeners = listeners.filter(l => l !== listener);
     };
   }
+
+  // Initialize the state
+  dispatch({});
+
+  return { getState, dispatch, subscribe };
+}
+
+// Create an instance of the store
+const store = createStore(reducer);
+
+// Subscribe to state changes and log the state to the console
+store.subscribe(() => {
+  console.log('State updated:', store.getState());
+});
+
+// Export the store and action types
+export { store, ADD, SUBTRACT, RESET };
