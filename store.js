43dfe72b39ca0f2469562e7ab985f2c33d48c651
kodegv <celsocharlez1@gmail.com>
2024-07-25 11:03:00 +0200

    defined reducer function

--- store.js
+++ store.js
@@ -7,3 +7,17 @@ const initialState = { count: 0 };
 const ADD = 'ADD';
 const SUBTRACT = 'SUBTRACT';
 const RESET = 'RESET';
+
+// Define the reducer function
+function reducer(state = initialState, action) {
+    switch (action.type) {
+      case ADD:
+        return { count: state.count + 1 };
+      case SUBTRACT:
+        return { count: state.count - 1 };
+      case RESET:
+        return { count: 0 };
+      default:
+        return state;
+    }
+  }
